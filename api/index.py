de461f9f08505c9635e654e68ea93ecb50c9ab1d
Siam Ahnaf <siamahnaf198@gmail.com>
2023-10-30 22:05:46 +0600

    Last try

--- api/index.py
+++ api/index.py
@@ -13,9 +13,9 @@ class APIAuthError(Exception):
 
 @app.route("/movies", methods=["POST"])
 def helloWorld():
-    movies_dict = pickle.load(open("./movie_dict.pkl", "rb"))
+    movies_dict = pickle.load(open("/movie_dict.pkl", "rb"))
     movies = pd.DataFrame(movies_dict)
-    similarity = pickle.load(open("./similarity.pkl", "rb"))
+    similarity = pickle.load(open("/similarity.pkl", "rb"))
     movie_name = request.json["movie_name"]
     movie_index = movies[movies["title"] == movie_name].index
     if len(movie_index) == 0:
